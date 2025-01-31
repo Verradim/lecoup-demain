8f0bed4818768e41f3dddaba9e2ebbddf241232d
Verradim <40742186+Verradim@users.noreply.github.com>
2025-01-31 10:44:46 +0100

    Reassurances UI 4

--- src/components/artisan-form/ReassuranceTexts.tsx
+++ src/components/artisan-form/ReassuranceTexts.tsx
@@ -1,15 +1,15 @@
 export const ReassuranceTexts = () => {
   return (
     <div className="hidden lg:flex flex-col gap-4 absolute inset-0 p-8">
-      <button className="bg-[#302cd7] transition-colors text-[#302cd71a] px-6 py-3 rounded-lg shadow-lg w-fit text-sm font-medium self-start mt-12">
+      <button className="bg-[#302cd7] transition-colors text-[#ffffff] px-6 py-3 rounded-lg shadow-lg w-fit text-sm font-medium self-start mt-12">
       Travaillez avec des professionnels de confiance, sélectionnés selon vos critères.
       </button>
       
-      <button className="bg-[#302cd7] transition-colors text-[#302cd71a] px-6 py-3 rounded-lg shadow-lg w-fit text-sm font-medium self-end mt-24">
+      <button className="bg-[#302cd7] transition-colors text-[#ffffff] px-6 py-3 rounded-lg shadow-lg w-fit text-sm font-medium self-end mt-24">
         Tous les artisans sont vérifiés et qualifiés avant d'être proposés.
       </button>
       
-      <button className="bg-[#302cd7] transition-colors text-[#302cd71a] px-6 py-3 rounded-lg shadow-lg w-fit text-sm font-small self-end mt-24">
+      <button className="bg-[#302cd7] transition-colors text-[#ffffff] px-6 py-3 rounded-lg shadow-lg w-fit text-sm font-small self-end mt-24">
       Notre équipe vous accompagne à chaque étape pour trouver le bon partenaire.
       </button>
     </div>
